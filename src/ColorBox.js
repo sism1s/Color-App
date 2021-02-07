7e0fdf78b4332fc30525dba8c2bc7a04376dadda
sism1s <marko.lazic880@gmail.com>
2021-02-07 18:33:03 +0000

    update

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import "./ColorBox.css";
 
 class ColorBox extends Component {
   render() {
     return (
       <div style={{ background: this.props.background }} className="ColorBox">
+        <span>{this.props.name}</span>
         <span>More</span>
       </div>
     );
