d76ce00c6271a45775949a37e2cf242fa8e1bdd1
sism1s <marko.lazic880@gmail.com>
2021-02-13 00:00:06 +0000

    Make colorBox responsive

--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,3 +1,5 @@
+import sizes from "./sizes";
+
 export default {
   palette: {
     height: "100vh",
@@ -38,6 +40,18 @@ export default {
       textTransform: "uppercase",
       border: "none",
       textDecoration: "none",
+    },
+    [sizes.down("lg")]: {
+      width: "75%",
+      height: "33.3333%",
+    },
+    [sizes.down("md")]: {
+      width: "50%",
+      height: "20%",
+    },
+    [sizes.down("xs")]: {
+      width: "100%",
+      height: "10%",
     },
   },
 };
